d729be61d6de5930377e3d4332eb6fa94737d6dd
Dev.RabbitZz 🥕 <156213745+ksusXLabs@users.noreply.github.com>
2026-05-16 10:49:10 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PREFIX: "/",
     AUTO_READ_STATUS: true,
-    AUTO_REACT: false,
+    AUTO_REACT: true,
     AUTO_TYPING: false,
     REACT_EMOJIS: ['❤','💕','😻','🧡','💛','💚','💙','💜','🎉','👋']
 };
